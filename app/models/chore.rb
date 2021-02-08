e1a332c69e234ccd99673c2da4e20b8f597cc3fe
claudia0sorio <claudiaaa1414@gmail.com>
2021-02-07 20:01:44 -0500

    fixed chores view and added methods

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,11 @@
 class Chore < ApplicationRecord
     belongs_to :child
     belongs_to :task
+
+    scope :chronological, -> { order('due_on DESC') }
+    scope :by_task, -> { joins(:task).order('name DESC') }
+    scope :done, -> { where(completed:true) }
+
+
+
 end
